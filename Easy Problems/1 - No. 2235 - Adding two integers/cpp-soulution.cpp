9dc8aea722fa426218c57110a94c464b85a7971b
Divyansh Bhandari <90455204+bh-divyansh@users.noreply.github.com>
2023-01-08 17:42:19 +0530

    Solved problem 2235

---
+++ Easy Problems/1 - No. 2235 - Adding two integers/cpp-soulution.cpp
@@ -0,0 +1,10 @@
+// Problem link: https://leetcode.com/problems/add-two-integers/
+// Solving date: 8 Jan 2023
+class Solution
+{
+public:
+    int sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+};
